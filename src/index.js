7ca07a8ce8f31048af5424c5718b5930b0b1323d
Sheldrick Mayrant <mrmayrant@yahoo.com>
2022-06-25 20:29:50 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/main.scss";
+import darkLogo from "./img/logo-dark.png";
+
+const logoDark = document.querySelector(".header__logo-img");
+logoDark.src = darkLogo;
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+    splitChunks: { chunks: "all" },
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "src"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+        include: path.resolve(__dirname, "src/styles"),
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            include: path.resolve(__dirname, "src"),
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g)$/i,
+        type: "asset/resource",
+        include: path.resolve(__dirname, "src/img"),
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+      chunks: ["index"],
+    }),
+    new MiniCssExtractPlugin(),
+    require("autoprefixer"),
+  ],
+};
